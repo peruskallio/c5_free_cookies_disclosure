1f05b7fb7a9bb9f8500a13a4bf3331b202157ac6
Pavel Yakimov <pavel@yakimoff.ru>
2015-09-08 18:10:47 +0700

    Refactored session\cookie code

--- src/CookieAllowance.php
+++ src/CookieAllowance.php
@@ -1,6 +1,9 @@
 <?php
 namespace Concrete\Package\FreeCookiesDisclosure\Src;
 
+use Concrete\Core\Http\Request;
+use Concrete\Core\Support\Facade\Application as Core;
+
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
 class CookieAllowance
@@ -8,9 +11,18 @@ class CookieAllowance
 
     public function cookiesAllowed()
     {
-        if (!isset($_SESSION['cookies_allowed'])) {
-            if (isset($_COOKIE['cookies_allowed']) && $_COOKIE['cookies_allowed'] == true) {
-                $_SESSION['cookies_allowed'] = true;
+        $r = Request::getInstance();
+        $session = $r->getSession();
+
+        if (!$session) {
+            $session = Core::make('session');
+        }
+
+        if (!$session->has('cookies_allowed')) {
+            $cookie = Core::make('cookie');
+
+            if ($cookie->has('cookies_allowed') && $cookie->get('cookies_allowed') == true) {
+                $session->set('cookies_allowed', true);
 
                 return true;
             }
@@ -18,7 +30,7 @@ class CookieAllowance
             return false;
         }
 
-        return $_SESSION['cookies_allowed'] == true;
+        return $session->get('cookies_allowed') == true;
     }
 
 }
