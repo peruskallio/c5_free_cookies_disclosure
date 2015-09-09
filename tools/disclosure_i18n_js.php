1654cfeabeaf83ae42b7f7412d2909064b6e159a
Pavel Yakimov <pavel@yakimoff.ru>
2015-09-09 14:17:16 +0700

    Deleted not used tool
    JS previously provided by this tool is in package service provider now.

--- tools/disclosure_i18n_js.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('C5_EXECUTE') or die("Access Denied.");
-
-header('Content-type: text/javascript');
-?>
-var ccmi18n_cookiesdisclosure = {
-    allowCookies: '<?php echo t("You need to allow cookies for this site!") ?>'
-};
